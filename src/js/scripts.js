f4fb9caaea13cee9e932226b6ca6f0408e77cf51
Jonah <jonah.declerck@student.kdg.be>
2023-03-04 23:03:17 +0100

    color changing sphere go brrr

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui';
 
 //renerer
 
@@ -34,15 +35,54 @@ orbit.update();
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-//shape
+//cube
 
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = 2;
 scene.add(box);
 
-//rotation
+//plane
 
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = -0.5 * Math.PI;
+scene.add(plane);
+
+//sphere
+
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
+const sphereMaterial = new THREE.MeshBasicMaterial({
+    color: 0x0000FF,
+    wireframe: false
+});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.set(-10, 10, 0);
+scene.add(sphere);
+
+//gridhelper
+
+const gridHelper = new THREE.GridHelper(30, 10);
+scene.add(gridHelper);
+
+//dat gui
+
+const gui = new dat.GUI();
+
+const options = {
+    sphereColor: '#ffea00'
+}
+
+gui.addColor(options, 'sphereColor').onChange(function(e){
+    sphere.material.color.set(e);
+});
+
+//rotation
 
 function animate(){
     box.rotation.x += 0.01;
